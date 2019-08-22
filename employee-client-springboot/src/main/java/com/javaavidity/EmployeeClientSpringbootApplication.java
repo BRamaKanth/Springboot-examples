d7c0869ae4040801e9e7ee378a6a0a42b9177b23
ramakanthb <ramakanthb@ERL011021895.infics.com>
2019-08-22 11:43:08 +0530

    initial commit

---
+++ employee-client-springboot/src/main/java/com/javaavidity/EmployeeClientSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.javaavidity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeClientSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeClientSpringbootApplication.class, args);
+	}
+
+}
